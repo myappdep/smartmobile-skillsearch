0c04bff8d1220a28421e1baefff6ce7b1f8684f0
Prashant Kumar Shukla <prashant.k.shukla@accenture.com>
2019-07-13 00:25:41 +0530

    second commit

--- data/stores.js
+++ data/stores.js
@@ -12,24 +12,24 @@ const stores = [
     servicename: 'Azure PaaS',
     contact: '9686449798',
     SME: 'prashant',
-    capabilitylead ='Babula',
-    emailId = 'prashant.k.shukla@accenture.com'
+    capabilitylead :'Babula',
+    emailId : 'prashant.k.shukla@accenture.com'
   },
   {
     id: 3,
     servicename: 'Azure storage',
     contact: '9686449798',
     SME: 'Suvendu',
-    capabilitylead ='Babula',
-    emailId = 'suvendu.mohanty@accenture.com'
+    capabilitylead :'Babula',
+    emailId : 'suvendu.mohanty@accenture.com'
   },
   {
     id: 4,
     servicename: 'Azure Networking',
     contact: '9686449798',
     SME: 'Kedara',
-    capabilitylead ='Babula',
-    emailId = 'suvendu.mohanty@accenture.com'
+    capabilitylead :'Babula',
+    emailId : 'suvendu.mohanty@accenture.com'
   }
 ];
 
